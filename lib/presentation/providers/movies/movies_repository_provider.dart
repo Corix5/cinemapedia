94850b0ef6736b2a7f71e0722b84ade509800577
Corix5 <luisdavid202075@gmail.com>
2025-07-10 14:06:41 -0600

    Cierre de arquitectura primera fase

--- lib/presentation/providers/movies/movies_repository_provider.dart
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -1,5 +1,5 @@
 import 'package:cinemapedia/infrastructure/datasources/moviedeb_datasource.dart';
-import 'package:cinemapedia/infrastructure/movie_repository_impl.dart';
+import 'package:cinemapedia/infrastructure/repositories/movie_repository_impl.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // Este repositorio es inmutable
